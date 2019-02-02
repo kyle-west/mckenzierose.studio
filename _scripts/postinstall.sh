c41e915530dceebb62a5b7eeb87155ec0dbd574f
Kyle West <kyle@familysearch.org>
2019-02-01 18:51:19 -0700

    [CONTENT] installed content from kyle-west/mckenzierose.studio-content

---
+++ _scripts/postinstall.sh
@@ -0,0 +1,8 @@
+mkdir content
+
+mv node_modules/mckenzierose.studio-content/images content/images
+mv node_modules/mckenzierose.studio-content/data-urls content/data-urls
+mv node_modules/mckenzierose.studio-content/asset.manifest.json content/asset.manifest.json
+
+
+rm -rf node_modules
