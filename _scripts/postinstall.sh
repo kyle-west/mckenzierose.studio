20c2c794300a8311fb23bba985a4ea133a9ec7f5
Kyle West <kyle@familysearch.org>
2019-03-02 08:50:19 -0700

    [IMAGES] use minified images

--- _scripts/postinstall.sh
+++ _scripts/postinstall.sh
@@ -1,6 +1,7 @@
 mkdir content
 
 mv node_modules/mckenzierose.studio-content/images content/images
+mv node_modules/mckenzierose.studio-content/min content/min
 mv node_modules/mckenzierose.studio-content/data-urls content/data-urls
 mv node_modules/mckenzierose.studio-content/asset.manifest.json content/asset.manifest.json
 
