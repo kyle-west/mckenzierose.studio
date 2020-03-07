d9445c3fc64f4cafbdd35b6f233599ad4d8987fb
Kyle West <kyle-west@users.noreply.github.com>
2020-03-06 17:59:25 -0700

    [2020.Q1] update images and use better masonry layout (#13)

--- _scripts/postinstall.sh
+++ _scripts/postinstall.sh
@@ -5,5 +5,4 @@ mv node_modules/mckenzierose.studio-content/min content/min
 mv node_modules/mckenzierose.studio-content/data-urls content/data-urls
 mv node_modules/mckenzierose.studio-content/asset.manifest.json content/asset.manifest.json
 
-
 rm -rf node_modules
