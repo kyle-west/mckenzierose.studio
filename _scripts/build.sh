ea0e64dde0fecda7cb1280e352c1a7573bc77ce2
Kyle West <kylewestrrr@gmail.com>
2021-02-06 09:56:27 -0700

    [contact] update bio info

--- _scripts/build.sh
+++ _scripts/build.sh
@@ -1,6 +1,10 @@
 rm index.html main.css main.js 2>/dev/null
 
-python3 _scripts/build-html.py
+if command -v python3; then 
+  python3 _scripts/build-html.py
+else 
+  python _scripts/build-html.py
+fi
 
 if [ "$1" == "--production" ]; then
   [ "$2" != '--silent' ] && echo "Creating Production Build..."
